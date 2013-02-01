aadabd3357c135a69e09c0083ec3a9649b04e3f6
Shane Yu <shanesimpkins@gmail.com>
2013-02-01 12:45:48 +0000

    Added extra customization options and cleaned up
    Added extra customization options and cleaned up some of the code
    indentation to make it more easiliy readable.

--- codemirror/lang/th.js
+++ codemirror/lang/th.js
@@ -3,5 +3,8 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'codemirror', 'th', {
-	toolbar: 'ดูรหัส HTML'
+	toolbar: 'ดูรหัส HTML',
+	autoFormatl: 'AutoFormat',
+	commentSelectedRange: 'Comment Selection',
+	uncommentSelectedRange: 'Uncomment Selection'
 });
