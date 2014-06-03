d408eccffbe0857d07f4e5e4bd310e89101702d8
Wiktor Walc <w.walc@cksource.com>
2014-06-03 10:44:02 +0200

    #57 CodeMirror icons are missing in release version

--- codemirror/plugin.js
+++ codemirror/plugin.js
@@ -8,7 +8,7 @@
 
 (function() {
     CKEDITOR.plugins.add('codemirror', {
-        icons: 'SearchCode,AutoFormat,CommentSelectedRange,UncommentSelectedRange,AutoComplete',
+        icons: 'searchcode,autoformat,commentselectedrange,uncommentselectedrange,autocomplete',
         lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en-au,en-ca,en-gb,en,eo,es,et,eu,fa,fi,fo,fr-ca,fr,gl,gu,he,hi,hr,hu,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt-br,pt,ro,ru,sk,sl,sr-latn,sr,sv,th,tr,ug,uk,vi,zh-cn,zh', // %REMOVE_LINE_CORE%
         version: 1.11,
         init: function (editor) {
