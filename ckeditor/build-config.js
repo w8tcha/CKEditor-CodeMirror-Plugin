6dfe272712c1a997e762ba817b68d0b9fa372b7e
w8tcha <thewatcher@watchersnet.de>
2019-05-06 15:29:41 +0200

    updated to latest version

--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -1,6 +1,6 @@
 ﻿/**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
 /**
@@ -10,13 +10,13 @@
  * If you would like to build CKEditor online again
  * (for example to upgrade), visit one the following links:
  *
- * (1) http://ckeditor.com/builder
+ * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/50877b4c1fcbadd4a2c7659a7907c067
+ * (2) https://ckeditor.com/cke4/builder/1d0359db09fa8e3128004bdc70eba0e3
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/50877b4c1fcbadd4a2c7659a7907c067
+ * (3) https://ckeditor.com/cke4/builder/download/1d0359db09fa8e3128004bdc70eba0e3
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,89 +26,43 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'moono-lisa',
-	preset: 'full',
+	preset: 'basic',
 	ignore: [
-		'.bender',
-		'bender.js',
-		'bender-err.log',
-		'bender-out.log',
-		'dev',
 		'.DS_Store',
+		'.bender',
 		'.editorconfig',
 		'.gitattributes',
 		'.gitignore',
-		'gruntfile.js',
 		'.idea',
 		'.jscsrc',
 		'.jshintignore',
 		'.jshintrc',
-		'less',
 		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
 		'node_modules',
 		'package.json',
-		'README.md',
 		'tests'
 	],
 	plugins : {
-		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
-		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
-		'contextmenu' : 1,
-		'copyformatting' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
-		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
-		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
-		'format' : 1,
-		'forms' : 1,
-		'horizontalrule' : 1,
-		'htmlwriter' : 1,
-		'iframe' : 1,
-		'image' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
-		'language' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
-		'magicline' : 1,
-		'maximize' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
-		'pastefromword' : 1,
-		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
-		'removeformat' : 1,
-		'resize' : 1,
-		'save' : 1,
-		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
-		'showborders' : 1,
-		'smiley' : 1,
-		'sourcearea' : 1,
-		'specialchar' : 1,
-		'stylescombo' : 1,
-		'tab' : 1,
-		'table' : 1,
-		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
-		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
@@ -131,6 +85,7 @@ var CKBUILDER_CONFIG = {
 		'en-gb' : 1,
 		'eo' : 1,
 		'es' : 1,
+		'es-mx' : 1,
 		'et' : 1,
 		'eu' : 1,
 		'fa' : 1,
