f2f32135c5372c4824deea81ec827861a568b937
Ingo <thewatcher@watchersnet.de>
2023-07-26 08:56:11 +0200

    updated packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,7 +369,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-cssmin");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-dev-update");
+	grunt.loadNpmTasks("@w8tcha/grunt-dev-update");
 
     grunt.registerTask("watch",
         [
